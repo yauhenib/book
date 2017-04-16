97aeeff29d18ac42cede7b182d5f713479095fb2
Vasili Svirski <vasili.svirski@gmail.com>
2017-04-16 13:47:58 +0300

    T0008: Add user validation helper test * implement user validation helper test
    Tested:
    * mvn clean package

--- book-service/src/test/java/com/igels/book/service/UserValidationHelperTest.java
+++ book-service/src/test/java/com/igels/book/service/UserValidationHelperTest.java
@@ -1,5 +1,33 @@
 package com.igels.book.service;
 
-class UserValidationHelperTest {
-    // TODO
+import com.igels.book.entity.UserInfo;
+import com.igels.book.exceptions.DataValidationException;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class UserValidationHelperTest {
+
+    @Test
+    public void testValidateId() throws DataValidationException {
+        UserValidationHelper.validateId(1);
+        try {
+            UserValidationHelper.validateId(-1);
+            fail("-1 user id no throw.");
+        }
+        catch (DataValidationException e) {
+        }
+    }
+
+    @Test
+    public void testValidateUser() throws DataValidationException {
+        UserInfo userInfo = new UserInfo(1);
+
+        try {
+            UserValidationHelper.validateUser(userInfo);
+            fail("User without name no throw.");
+        }
+        catch (DataValidationException e) {
+        }
+    }
 }
